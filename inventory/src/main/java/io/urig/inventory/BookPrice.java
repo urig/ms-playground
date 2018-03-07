28dfff05494c41efb0db340dc005e43d8cebfbd7
Uri Goldstein <uri.goldstein@gmail.com>
2018-03-07 22:34:38 +0200

    Inventory now gets prices from pricing!

---
+++ inventory/src/main/java/io/urig/inventory/BookPrice.java
@@ -0,0 +1,29 @@
+package io.urig.inventory;
+
+import java.math.BigDecimal;
+
+public class BookPrice {
+	private long bookId;
+	private BigDecimal price;
+	
+	public BookPrice() {};
+	
+	public BookPrice(long bookId, BigDecimal price) {
+		super();
+		this.bookId = bookId;
+		this.price = price;
+	}
+
+	public long getBookId() {
+		return bookId;
+	}
+	public void setBookId(long bookId) {
+		this.bookId = bookId;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+}
--- inventory/src/main/java/io/urig/inventory/InventoryRestController.java
+++ inventory/src/main/java/io/urig/inventory/InventoryRestController.java
@@ -1,11 +1,16 @@
 package io.urig.inventory;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/books")
@@ -21,7 +26,31 @@ public class InventoryRestController {
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Book> GetBooks() {
-		return bookRepository.findAll();
+		int customerId = 1;
+		
+		List<Book> books = bookRepository.findAll();
+		
+		String bookIds = books.stream()
+				.map(Book::getId)
+				.map(id->id.toString())
+				.collect(Collectors.joining(","));
+		
+		RestTemplate restTemplate = new RestTemplate();
+        BookPrice[] bookPrices = restTemplate.getForObject("http://localhost:8090/pricing/customer/1?bookIds=" + bookIds, BookPrice[].class);
+
+		
+		/*
+		ResponseEntity<List<BookPrice>> rateResponse =
+				restTemplate.exchange(
+						"http://localhost:8090/pricing/customer/1?bookIds="+bookIds, 
+						HttpMethod.GET, 
+						null, 
+						new ParameterizedTypeReference<List<BookPrice>>() {});
+		List<BookPrice> bookPrices = rateResponse.getBody();
+        */
+        //List<BookPrice> bookPrices = restTemplate.getForObject("http://localhost:8090/pricing/customer/1?bookIds=" + bookIds, List<BookPrice>.class);
+        //log.info(quote.toString());
+		return books;
 	}
 	
 }
