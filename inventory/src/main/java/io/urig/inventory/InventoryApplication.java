4dc842b9de2aac4e654604f99e2c53da7bea59c2
Uri Goldstein <uri.goldstein@gmail.com>
2018-03-07 21:40:51 +0200

    Adding Spring Retry to Inventory service

--- inventory/src/main/java/io/urig/inventory/InventoryApplication.java
+++ inventory/src/main/java/io/urig/inventory/InventoryApplication.java
@@ -4,17 +4,19 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class InventoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
 	}
-	
+
 	@Bean
 	public CommandLineRunner getInit(BookRepository bookRepository) {
-		return (evt) -> bookRepository.save(new Book(1, "Moby Dick", "Herman Melville"));
+		return (args) -> bookRepository.save(new Book(1, "Moby Dick", "Herman Melville"));
 	}
 
 }
