f37445f5f1beaac92d9b8d05ec462baa219ead46
Uri Goldstein <uri.goldstein@gmail.com>
2018-03-09 13:53:32 +0200

    Removed some unused imports

--- pricing/src/main/java/io/urig/pricing/PricingRestController.java
+++ pricing/src/main/java/io/urig/pricing/PricingRestController.java
@@ -1,12 +1,7 @@
 package io.urig.pricing;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
