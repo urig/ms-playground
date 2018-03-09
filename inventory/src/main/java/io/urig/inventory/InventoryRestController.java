717fc4a6210f844f339d017131d3c9c80613f5d7
Uri Goldstein <uri.goldstein@gmail.com>
2018-03-09 15:56:05 +0200

    Inventory no longer goes to pricing

--- inventory/src/main/java/io/urig/inventory/InventoryRestController.java
+++ inventory/src/main/java/io/urig/inventory/InventoryRestController.java
@@ -1,9 +1,12 @@
 package io.urig.inventory;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,19 +24,23 @@ public class InventoryRestController {
 		this.bookRepository = bookRepository;
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping()
 	public List<Book> GetBooks() {
 		int customerId = 1;
 
 		List<Book> books = bookRepository.findAll();
 
-		String bookIds = books.stream().map(Book::getId).map(id -> id.toString()).collect(Collectors.joining(","));
+//		String bookIds = books.stream().map(Book::getId).map(id -> id.toString()).collect(Collectors.joining(","));
+//		RestTemplate restTemplate = new RestTemplate();
+//		BookPrice[] bookPrices = restTemplate.getForObject(
+//				"http://localhost:8090/pricing/customer/" + customerId + "?bookIds=" + bookIds, BookPrice[].class);
 
-		RestTemplate restTemplate = new RestTemplate();
-		BookPrice[] bookPrices = restTemplate.getForObject(
-				"http://localhost:8090/pricing/customer/" + customerId + "?bookIds=" + bookIds, BookPrice[].class);
-		
 		return books;
+	}
+	
+	@GetMapping(path="/{bookId}")
+	public Optional<Book> getBookById(@PathVariable Long bookId) {
+		return bookRepository.findById(bookId);
 	}
 
 }
